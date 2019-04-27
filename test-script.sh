c4d9962bac88dbe737da129bc8ecb6dc2bd09d47
mdulipati <mdulipati@fb.com>
2019-04-27 07:59:12 -0700

    adding new lines to test-script

--- test-script.sh
+++ test-script.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+o#!/bin/bash
 
 echo "enter your name"
 read name
 
 echo "Name of the user is $name"
-
+if [[ $name == "mani" ]]; then
+    echo "user is mani"
+else
+    echo "unknown user"
+fi
